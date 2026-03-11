5cd688d819abe29f4c196a40d190548644f22e4a
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-03-11 10:37:10 -0300

    testa .service.impl

---
+++ src/main/java/br/com/ferdbgg/springestudoalura/service/impl/EnderecoServiceImpl.java
@@ -0,0 +1,83 @@
+package br.com.ferdbgg.springestudoalura.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import br.com.ferdbgg.springestudoalura.domain.entity.Endereco;
+import br.com.ferdbgg.springestudoalura.dto.request.DadosCadastroEndereco;
+import br.com.ferdbgg.springestudoalura.service.EnderecoService;
+
+@Service
+public class EnderecoServiceImpl implements EnderecoService{
+
+    @Override
+    public Endereco parseEndereco(DadosCadastroEndereco dados) {
+
+        if (dados == null) {
+            return null;
+        }
+
+        final Endereco endereco = new Endereco();
+
+        endereco.setLogradouro(dados.logradouro());
+
+        endereco.setBairro(dados.bairro());
+
+        endereco.setCep(dados.cep());
+
+        endereco.setCidade(dados.cidade());
+
+        endereco.setUf(dados.uf());
+
+        endereco.setComplemento(dados.complemento());
+
+        endereco.setNumero(dados.numero());
+
+        return endereco;
+
+    }
+
+    @Override
+    public String parseString(Endereco endereco) {
+
+        if (endereco == null) {
+            return null;
+        }
+        
+        final String virgula = ",";
+        final String espaco = " ";
+
+        StringBuilder buffer = new StringBuilder();
+        
+        buffer.append(endereco.getLogradouro());
+        buffer.append(virgula);
+        buffer.append(espaco);
+
+        final String numero = endereco.getNumero();
+        buffer.append(numero == null || numero.isBlank() ? "S/N" : numero);
+        buffer.append(virgula);
+        buffer.append(espaco);
+
+        final String complemento = endereco.getComplemento();
+        if (complemento != null && !complemento.isBlank()) {
+            buffer.append(complemento);
+            buffer.append(virgula);
+            buffer.append(espaco);
+        }
+
+        buffer.append(endereco.getBairro());
+        buffer.append(virgula);
+        buffer.append(espaco);
+
+        buffer.append(endereco.getCidade());
+        buffer.append("-");
+        buffer.append(endereco.getUf());
+        buffer.append(virgula);
+        buffer.append(espaco);
+
+        buffer.append(endereco.getCep());
+
+        return buffer.toString();
+
+    }
+
+}
