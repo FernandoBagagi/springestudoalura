a881b61fbb492a913027c80c98f6dc3a570a1915
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-08-13 11:37:29 -0300

    refatora DadosToken

--- src/main/java/br/com/ferdbgg/springestudoalura/domain/dto/response/DadosResponseToken.java
+++
@@ -1,5 +0,0 @@
-package br.com.ferdbgg.springestudoalura.domain.dto.response;
-
-public record DadosResponseToken(String token) {
-
-}
---
+++ src/main/java/br/com/ferdbgg/springestudoalura/model/api/response/DadosToken.java
@@ -0,0 +1,6 @@
+package br.com.ferdbgg.springestudoalura.model.api.response;
+
+public record DadosToken(
+        String token //
+) {
+}
